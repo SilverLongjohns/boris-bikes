68d1f5d57fe8ba05ebff6ff370208a2bfb335d14
EC2 Default User <ec2-user@ip-172-31-47-92.ec2.internal>
2020-07-08 16:01:27 +0000

    20 bike limit added

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,14 +1,18 @@
 require_relative 'bike'
 
 class DockingStation
-  attr_reader :bike
+  attr_reader :bikes
+    def initialize
+      @bikes = []
+    end
   
     def release_bike
-        raise "No bikes available" unless @bike
-        @bike
+        raise "No bikes available" unless @bikes.length > 0
+        @bikes.pop
     end
     
     def dock(bike)
-      @bike = bike
+      raise "Docking station full" if @bikes.length >= 20
+      @bikes << bike
     end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -13,16 +13,20 @@ describe DockingStation do
   it "raises an error when no bikes are available" do
     expect {subject.release_bike}.to raise_error "No bikes available"
   end
+  
+  it "raises an error when docking station is full" do
+    20.times { subject.dock Bike.new }
+    expect{subject.dock Bike.new}.to raise_error "Docking station full"
+  end
     
   it "docks a bike" do
-    bike = Bike.new
-    subject.dock(bike)
+    bike = subject.dock(bike)
     expect(subject.dock(bike)).to eq bike
   end
   
   it "checks for a docked bike" do
     bike = Bike.new
     subject.dock(bike)
-    expect(subject.bike).to eq bike
+    expect(subject.bikes[0]).to eq bike
   end
 end
